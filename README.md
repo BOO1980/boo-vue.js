b28a7da688c9e0fd8bb362eca1900f8a3158ec8c
Hayley <hb@hungrybeargaming.co.uk>
2023-03-22 15:10:19 +0000

    Vue - Count app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # boo-vue.js
 ## training tracker
-https://vuejs.org/tutorial/#step-2
+https://vuejs.org/guide/introduction.html#what-is-vue
+https://vuejs.org/tutorial/#step-1 - need to do again
+https://vuejs.org/tutorial/#step-2 - need to do again
---
+++ tutorials/vuejs.org/0-what-is-vue/code/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="app">
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</div>
+  </template>
+  
+  <script>
+  export default {
+      data() {
+        return {
+        count: 0
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    color: blue;
+  }
+  </style>
